a796d4cf1bb065b49a3d262a577ab1ae2ff3d6e4
ghada <ghadaemad011@gmail.com>
2023-09-18 01:05:37 +0300

    set structure

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,27 +1,31 @@
-export const Images=(props)=>{
-    const {urls,username,des,likes}=props;
-    
-    // const res=props.props;
-    // const images=res.map((img)=>{
-    //     console.log(img.user.username)
-    // })
-    // const{urls}=props;
-    
-    // const res=props.props;
-    // res.map((url)=>{
-    //     console.log(url.user.username)
-    // })
-    // //console.log(props.props)
-    
-    
-    // //.log(props)
-    return(
-        <div className="flex gap-4">
-            <img src={urls} alt=""/>
-            <p>{username}</p>
-            <p>{des}</p>
-            <p>{likes}</p>
-            
+export const Images = (props) => {
+  const { urls, username, des, likes } = props;
+
+  // const res=props.props;
+  // const images=res.map((img)=>{
+  //     console.log(img.user.username)
+  // })
+  // const{urls}=props;
+
+  // const res=props.props;
+  // res.map((url)=>{
+  //     console.log(url.user.username)
+  // })
+  // //console.log(props.props)
+
+  // //.log(props)
+  return (
+    <div className="grid grid-flow-col">
+      <div className="flex">
+        <div>
+          <img src={urls} alt="" />
         </div>
-    )
-}
+        <div>
+          <p>{username}</p>
+          <p>{des}</p>
+          <p>{likes}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
