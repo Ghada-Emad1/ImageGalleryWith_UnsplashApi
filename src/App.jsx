cb87fa004a897cd9fdb526268a7fbeef8265695f
ghada <ghadaemad011@gmail.com>
2023-10-16 13:40:52 +0300

    exchange with react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Gallery } from "./components/Gallery";
 
 const App = () => {
+  const queryclient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: Infinity,
+        cacheTime: Infinity,
+      },
+    },
+  });
   return (
     <div>
-      <Gallery/>
+      <QueryClientProvider client={queryclient}>
+        <Gallery />
+      </QueryClientProvider>
     </div>
   );
 };
