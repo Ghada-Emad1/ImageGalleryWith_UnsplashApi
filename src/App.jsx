f3dbe882d23fb3b4910caa7b2bd760468fbb678e
ghada <ghadaemad011@gmail.com>
2023-10-16 20:16:58 +0300

    add more dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Gallery } from "./components/Gallery";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Details from "./components/Details";
 const App = () => {
   const queryclient = new QueryClient({
     defaultOptions: {
@@ -12,9 +13,14 @@ const App = () => {
   });
   return (
     <div>
-      <QueryClientProvider client={queryclient}>
-        <Gallery />
-      </QueryClientProvider>
+      <BrowserRouter>
+        <QueryClientProvider client={queryclient}>
+          <Gallery />
+          <Routes>
+            <Route path="/photodetail:id" element={<Details/>}/>
+          </Routes>
+        </QueryClientProvider>
+      </BrowserRouter>
     </div>
   );
 };
